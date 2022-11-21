d3fb327f42702690c9f7fa9fbfb3fc0584cf6212
Krreww <krreww@gmail.com>
2022-11-21 19:42:24 +0300

    Final Version* (3 levels)

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Level lvl1 = new Level("C:/Users/user/IdeaProjects/Maze/src/level1.txt");
-        Level lvl2 = new Level("C:/Users/user/IdeaProjects/Maze/src/level2.txt");
-        Player.play(lvl1);
-        Player.play(lvl2);
+        Level lvl1 = new Level("Levels/level1.txt");
+        Level lvl2 = new Level("Levels/level2.txt");
+        Level lvl3 = new Level("Levels/level3.txt");
+        Player.startGame(Level.levels);
     }
 }
